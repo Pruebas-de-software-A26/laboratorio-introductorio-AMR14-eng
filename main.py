32d113a767eda183f8875ba0b5149fc1313afa95
Arleth Misuki O.R. <111011019+AMR14-eng@users.noreply.github.com>
2026-08-21 01:30:20 +0000

    first test cases

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
-import operations
+import logging
+import operations  
 
-if __name__ == 'main':
-    
+logging.basicConfig(
+  level=logging.INFO,
+  filemode= "w",
+  filename="test.log"
+  )
+
+if __name__ == "__main__":
+
+    logging.info("TEST CASE 1")
+    result = operations.add(1, 2)
+
+    if result == 3:
+        logging.info("PASS")
+    else:
+        logging.error("FAIL")
+
+    logging.info("TEST CASE 2")
+    result2 = operations.power(2, 2)
+
+    if result2 == 4:
+        logging.info("PASS")
+    else:
+        logging.error("FAIL")
+
+
+
+  # logging.info(f"Result: {result}")
+  # logging.warning("Division Imposible")
+  # logging.error("Error")
+  # logging.debug("The value i a and b is...")
+  # logging.critical("Critical in time")
